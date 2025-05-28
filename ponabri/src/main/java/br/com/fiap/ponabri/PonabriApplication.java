16c5553802eb3274d7f1a14575570741887b7db3
TalytaBP <talyytta@gmail.com>
2025-05-28 17:24:14 -0300

    primeiro commit

---
+++ ponabri/src/main/java/br/com/fiap/ponabri/PonabriApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.ponabri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonabriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PonabriApplication.class, args);
+	}
+
+}
---
+++ ponabri/src/test/java/br/com/fiap/ponabri/PonabriApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.ponabri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PonabriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
